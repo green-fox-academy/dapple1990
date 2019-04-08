a5616dbb4bdccfa6db5c6a470446ea4c7154b293
Dapple1990 <bone.dalma@gmail.com>
2019-04-08 13:46:50 +0200

    exercise doubling

---
+++ home/dapple1990/greenfox/dapple1990/week-02/day-01/doubling.ts
@@ -0,0 +1,9 @@
+let baseNum: any = 123;
+
+function doubling(firstNumber: number, secondNumber: number): number {
+    return firstNumber * secondNumber;
+
+}
+baseNum = doubling(7,12)
+console.log(baseNum);
+
