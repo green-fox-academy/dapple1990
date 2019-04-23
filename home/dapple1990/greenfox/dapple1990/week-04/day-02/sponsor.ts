602d78d79f5a5737769de192b8e1ed2f31fcc61f
Dapple1990 <bone.dalma@gmail.com>
2019-04-23 16:49:40 +0200

    1. green fox organization

---
+++ home/dapple1990/greenfox/dapple1990/week-04/day-02/sponsor.ts
@@ -0,0 +1,27 @@
+'use strict';
+
+import { Person } from "./person";
+
+export class Sponsor extends Person {
+    company : string;
+    hiredStudents : number;
+
+    constructor(name : string = 'Jane Doe', age : number = 30, gender : string = 'female', company : string = 'Google', hiredStudents : number = 0) {
+        super(name, age, gender)
+        this.company = company;
+        this.hiredStudents = hiredStudents;
+    }
+
+    introudce() {
+        console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} who represents ${this.company} and hired ${this.hiredStudents} students so far.'`)
+    }
+
+    hire () {
+        this.hiredStudents -= 1;
+    }
+
+    getGoal() {
+        console.log('My goal is: Hire brilliant junior software developers.');
+    }
+}
+
