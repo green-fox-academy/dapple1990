8b5bfcfccaa44c776b62a84132f202faa13fcec8
Dapple1990 <bone.dalma@gmail.com>
2019-05-15 13:49:25 +0200

    finished exercise 1

---
+++ week-06/day-03/Queryselector-1/queryselector-1.js
@@ -0,0 +1,24 @@
+var storeTheKing = document.getElementById('b325')
+console.log(storeTheKing);
+
+var businessLamp = document.querySelectorAll('.big')
+for (var i = 0; i < businessLamp.length; i++) {
+  console.log(businessLamp[i]);
+}
+console.log(businessLamp);
+
+var conceitedKing = document.querySelectorAll('.container div');
+for (var i = 0; i < conceitedKing.length; i++) {
+  alert(conceitedKing[i].textContent);
+}
+
+
+var noBusiness = document.getElementsByTagName('div')
+for (let i = 0; i < noBusiness.length; i++) {
+  //console.log(noBusiness[i]);
+}
+
+
+
+
+
