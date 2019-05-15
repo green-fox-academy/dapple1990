1fa7e838d4de47d0738e64cff5a27384594c6c35
Dapple1990 <bone.dalma@gmail.com>
2019-05-15 19:43:54 +0200

    fixed content-2

--- week-06/day-03/Content-2/content-2.js
+++ week-06/day-03/Content-2/content-2.js
@@ -1,13 +1,15 @@
  var items = ['apple', 'banana', 'cat', 'dog']
  var listItems = document.getElementsByTagName('li');
 
- for(let i = 0; listItems.length; i++) {
-   for(let h = 0; h < items.length; h++){
+ for(let i = 0; i < listItems.length; i++) {
+   
     listItems[i].textContent = items[i];
    }
-  }
+  
 
   
   var ulColor = document.getElementsByTagName('ul');
-  ulColor[0].setAttribute('color', 'coloredUL');
+  ulColor[0].setAttribute('class', 'coloredUL');
   
+  /* 2) change the <ul> element's background color to 'limegreen'
+  - use css class to change the color instead of the style property */
