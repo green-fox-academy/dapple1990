b1e077c81522aa780d5c49a2942af8fae099d136
Dapple1990 <bone.dalma@gmail.com>
2019-05-23 21:56:19 +0200

    moved practice files

--- practice at home/parrot.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import { Animal } from './animal-protection';
-
-class Parrot extends Animal {
-    constructor(ownerName: string, isHealthy: boolean, healCost: number = 5, name: string) {
-        super(ownerName, isHealthy, healCost, name);
-}
